238e4b6fe0b266adf87d524f10704aa20bb6c5e1
iqrokristiadi <iqro.kristiadi2004@gmail.com>
2025-10-06 21:07:17 +0700

    update global search

--- controllers/globalSearchController.js
+++ controllers/globalSearchController.js
@@ -18,14 +18,48 @@ export const globalSearch = async (req, res) => {
 
     // Search across collections
     const [movies, directors, actors] = await Promise.all([
-      Movie.find({ title: regex }).select("title releaseDate -_id"),
-      Director.find({ name: regex }).select("name bio -_id"),
-      Actor.find({ name: regex }).select("name bio -_id"),
+      Movie.find({ title: regex })
+        .select("title releaseDate -_id")
+        .populate("director", "name -_id")
+        .populate("actors", "name -_id")
+        .populate("genre", "name -_id"),
+      Director.find({ name: regex }).select("name bio"),
+      Actor.find({ name: regex }).select("name bio"),
     ]);
+
+    // For each actor found, find their movies
+    const actorsWithMovies = await Promise.all(
+      actors.map(async (actor) => {
+        const actorMovies = await Movie.find({
+          actors: { $in: [actor._id] },
+        }).select("title releaseDate -_id");
+        return {
+          ...actor.toObject(),
+          movies: actorMovies,
+        };
+      })
+    );
+
+    // For each director found, find their movies
+    const directorsWithMovies = await Promise.all(
+      directors.map(async (d) => {
+        const directorMovies = await Movie.find({
+          director: d._id,
+        }).select("title releaseDate -_id");
+        return {
+          ...d.toObject(),
+          movies: directorMovies,
+        };
+      })
+    );
 
     res.json({
       success: true,
-      data: { movies, actors, directors },
+      data: {
+        movies,
+        actors: actorsWithMovies,
+        directors: directorsWithMovies,
+      },
     });
   } catch (error) {
     res.status(500).json({
